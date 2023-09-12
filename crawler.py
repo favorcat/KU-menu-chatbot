5224c2e2bae0a4033ea63ff61feadc9783e18c65
Eunyeong Ko <flower_hana39@naver.com>
2023-09-12 22:39:17 +0900

    edit: crawler
    식단표 예쁘게 출력하기

--- crawler.py
+++ crawler.py
@@ -3,8 +3,9 @@ from selenium.webdriver.common.by import By
 from datetime import datetime
 
 options = webdriver.ChromeOptions()
+options.add_argument("headless")
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
-driver = webdriver.Chrome()  # 크롬 드라이버 경로 설정
+driver = webdriver.Chrome(options=options)  # 크롬 드라이버 경로 설정
 
 driver.get("https://www.korea.ac.kr/user/restaurantMenuAllList.do?siteId=university&id=university_050402000000")
 
@@ -12,18 +13,47 @@ today = datetime.today().weekday()
 path = f'/html/body/div[4]/div[2]/div[2]/div[2]/div/ul/li[2]/ol[{today+1}]/li/div'
 
 menulist = driver.find_element(By.XPATH,path).text.split('[')
-studnet_menu = menulist[1].split('▶')
+student_menu = menulist[1].split('▶')
 staff_menu = menulist[2].split('▶')
 
+date = datetime.today().strftime("%m월 %d일 식단표")
+sendText = '❤️' + date + '❤️\n\n'
+
 #학생식당
-student_menulist = '[학생식당] - 5,000원\n'
-for i in range(1, len(studnet_menu)):
-  student_menulist += "\n▶" + studnet_menu[i]
+for i in range(1, len(student_menu)):
+  a = student_menu[i]
+  s = a.split(':',1)
+  if '조식' in a:
+    sendText += '1️⃣ 조식\n'
+    if '운영없음' in a:
+      sendText += '천원의 아침밥 - '
+    else:
+      sendText += '[운영시간 08:00~09:30]\n천원의 아침밥 - 🪪학생증 지참\n🍴'
+  elif '중식' in a:
+    sendText += '=\n2️⃣ 중식\n'
+    if '운영없음' in a:
+      sendText += '👩🏻‍🎓 학생식당 - '
+    else:
+      sendText += '👩🏻‍🎓 학생식당 - 💰5,000원\n[운영시간 11:00 ~ 13:30]\n🍴'
+  else:
+    break
+  sendText += s[1] + '\n'
 
 #교직원식당
-staff_menulist = '[교직원식당]\n'
 for i in range(1, len(staff_menu)):
-  staff_menulist += "\n▶" + staff_menu[i]
-
-sendText = student_menulist + "\n" + staff_menulist
+  a = staff_menu[i]
+  s = a.split(':',1)
+  if '중식' in a:
+    if '운영없음' in a:
+      sendText += '💁 교직원식당 - '
+    else:
+      sendText += '💁 교직원식당 - 💰6,500원\n[운영시간 11:00 ~ 14:00]\n🍴'
+  elif '석식' in a:
+    sendText += '=\n3️⃣ 석식\n'
+    if '운영없음' in a:
+      print("석식운영없음")
+      sendText += '💁 교직원식당 - '
+    else:
+      sendText += '💁 교직원식당 - 💰6,500원\n[운영시간 17:30~19:00]\n🍴'
+  sendText += s[1] + '\n'
 print(sendText)
